14e7654139826b2a23a9b5b4818b1f374d8ff985
russuiulia <russuiulia.md@gmail.com>
2021-10-07 10:48:19 +0300

    comentarii modificate

--- comentarii.java
+++ comentarii.java
@@ -1,16 +1,14 @@
-int d; // days until deadline
-// if the student is at least 18 years of age
+int daysUntilDeadline;
+
 if (student.Age> = 18)
 {
-    // send meeting invitation to the student
     notificationService.SendMessageTo(student, meetingInvitation);
 }
-else // if the student is younger than 18 years 
+else
 {
-    // sends a meeting invitation to the student's legal guardian
     notificationService.SendMessageTo(student.Parent, meetingInvitation);
 }
-// student is eligible for blood donation
+
 if (student.Age >= 17 && student.Weight >= 58.0 && student.Height >= 1.55)
 {
     ScheduleBloodDonatingSessionWith(student);
